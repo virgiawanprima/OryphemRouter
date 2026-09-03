af36f268134215cac307cb810138779bd84446b7
virgiawanprima <virgiawanprimarizky@gmail.com>
2026-09-03 09:29:52 +0800

    feat(open_sse_providers_registry): add open-sse/providers/registry/gitlawb.js

---
+++ open-sse/providers/registry/gitlawb.js
@@ -0,0 +1,22 @@
+export default {
+  id: "gitlawb",
+  priority: 300,
+  alias: "glb",
+  
+  display: {
+    name: "GitLaw",
+    color: "#64748B",
+    textIcon: "GI",
+  },
+  category: "apikey",
+  transport: {
+    baseUrl: "https://opengateway.gitlawb.com/v1/xiaomi-mimo",
+    
+  },
+  
+  models: [],
+  features: {
+    usage: true,
+    usageApikey: true,
+  },
+};
