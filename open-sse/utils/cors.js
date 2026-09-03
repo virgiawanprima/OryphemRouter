76c15e1eb48c727bf387eabffa6f7541f67479a2
virgiawanprima <virgiawanprimarizky@gmail.com>
2026-09-03 09:30:15 +0800

    feat(open_sse_utils): add open-sse/utils/cors.js

---
+++ open-sse/utils/cors.js
@@ -0,0 +1,7 @@
+const CORS_HEADERS = {
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, x-api-key, anthropic-version, x-omniroute-connection, x-internal-test, accept"
+};
+export {
+  CORS_HEADERS
+};
